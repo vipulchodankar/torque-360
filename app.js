5df83a1f76c6424fc22e535f42d1c7b9e8f0fb78
Vipul Chodankar <vipul72c@gmail.com>
2019-10-01 15:21:21 +0530

    Added Root Route

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+// EJS SETUP
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Static Assets
+app.use(express.static("public"));
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+app.use("/", require("./routes/index"));
 
 const PORT = process.env.PORT || 6969;
 
 app.listen(PORT, function() {
-  console.log("Application is running at http://localhost:${PORT}/");
+  console.log(`Application is running at http://localhost:${PORT}/`);
 });
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("welcome");
+});
+
+// For invalid URLS
+router.get("*", (req, res) => {
+  res.render("404");
+});
+
+module.exports = router;

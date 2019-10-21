5738c014642d18af4a4eb73aefaf49a120ee801d
Vipul Chodankar <vipul72c@gmail.com>
2019-10-21 10:42:57 +0530

    Added fields in car addition

--- routes/cars.js
+++ routes/cars.js
@@ -12,26 +12,60 @@ router.get("/view", (req, res) => {
 });
 
 router.post("/add", (req, res) => {
-  const { model, company } = req.body;
+  const {
+    model,
+    company,
+    mileage,
+    cylinders,
+    clearance,
+    horsepower,
+    topspeed,
+    airbags,
+    fueltype
+  } = req.body;
 
   let errors = [];
   // Check Required Fields
-  if ( false ) {
+  if (
+    !model ||
+    !company ||
+    !mileage ||
+    !cylinders ||
+    !clearance ||
+    !horsepower ||
+    !topspeed ||
+    !airbags ||
+    !fueltype
+  ) {
     errors.push({ msg: "Please fill in all fields." });
   }
 
   if (errors.length > 0) {
-    // Send data back to register view
+    // Send data back to addCar view
     res.render("addCar", {
       errors,
       model,
-      company
+      company,
+      mileage,
+      cylinders,
+      clearance,
+      horsepower,
+      topspeed,
+      airbags,
+      fueltype
     });
   } else {
     // Create Car
     const newCar = new Cars({
       model,
-      company
+      company,
+      mileage,
+      cylinders,
+      clearance,
+      horsepower,
+      topspeed,
+      airbags,
+      fueltype
     });
 
     newCar

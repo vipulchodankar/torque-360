01139b78e4b250c2441ea23d463cdcbd569419cc
Vipul Chodankar <vipul72c@gmail.com>
2019-10-22 01:03:14 +0530

    Formatted Code, Added Back Button

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,7 @@
-dbPassword ="mongodb+srv://vipul:"+encodeURIComponent("38q4dehJtL0x8I3e")+"@cluster0-kqlye.mongodb.net/test?retryWrites=true&w=majority";
+dbPassword =
+  "mongodb+srv://vipul:" +
+  encodeURIComponent("38q4dehJtL0x8I3e") +
+  "@cluster0-kqlye.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = {
   MongoURI: dbPassword

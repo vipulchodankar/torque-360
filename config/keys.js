9183a999c965c90b5f21b5cfa4ba2ba390b03693
Vipul Chodankar <vipul72c@gmail.com>
2019-10-21 10:27:34 +0530

    Added mongodb credentials temporarilt

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword ="";
+dbPassword ="mongodb+srv://vipul:"+encodeURIComponent("38q4dehJtL0x8I3e")+"@cluster0-kqlye.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = {
   MongoURI: dbPassword
